97a1de34b1169646bab52bc96508970c94936452
iarbelaezc <163176799+iarbelaezc@users.noreply.github.com>
2024-04-16 14:18:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,8 @@ if img_file_buffer is not None:
       st.header('Isa Seria, con Probabilidad: '+str( prediction[0][0]) )
     if prediction[0][1]>0.5:
       st.header('Isa sonriente, con Probabilidad: '+str( prediction[0][1]))
+    if prediction[0][2]>0.5:
+      st.header('Isa con brillito, con Probabilidad: '+str( prediction[0][1]))
     #if prediction[0][2]>0.5:
     # st.header('Derecha, con Probabilidad: '+str( prediction[0][2]))
 
